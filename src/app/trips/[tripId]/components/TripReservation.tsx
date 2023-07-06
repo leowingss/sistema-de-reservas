898f2e2d3332f06ccb5f8dd98a564af3f110ff56
Leonardo <leondani853@gmail.com>
2023-07-06 18:24:46 -0300

    feat: add required validation to trip reservation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -5,21 +5,95 @@ import DatePicker from '@/components/DatePicker';
 import Input from '@/components/Input';
 import { Trip } from '@prisma/client';
 import Button from '@/components/Button';
+import { useForm, Controller } from 'react-hook-form';
 
 interface TripReservationProps {
     trip: Trip
 }
 
+interface TripReservationForm {
+    guests: number;
+    startDate?: Date | null;
+    endDate?: Date | null;
+}
+
 const TripReservation = ({ trip }: TripReservationProps) => {
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        control
+    } = useForm<TripReservationForm>();
+
+    const onSubmit = (data: any) => {
+        console.log({ data });
+    }
 
     return (
         <div className='flex flex-col p-5 pb-1'>
             <div className="flex gap-4">
-                <DatePicker className='w-full' placeholderText='Data de Início' onChange={() => { }} />
-                <DatePicker className='w-full' placeholderText='Data Final' onChange={() => { }} />
+
+                <Controller
+                    name='startDate'
+                    rules={{
+                        required: {
+                            value: true,
+                            message: 'Data inicial é obrigatória'
+                        }
+                    }}
+                    control={control}
+                    render={
+                        ({ field }) => (
+                            <DatePicker
+                                error={!!errors?.startDate}
+                                selected={field.value}
+                                errorMessage={errors?.startDate?.message}
+                                onChange={field.onChange}
+                                className='w-full'
+                                placeholderText='Data de Início'
+
+                            />
+                        )}
+                />
+
+                <Controller
+                    name='endDate'
+                    rules={{
+                        required: {
+                            value: true,
+                            message: 'Data final é obrigatória'
+                        }
+                    }}
+                    control={control}
+                    render={
+                        ({ field }) => (
+                            <DatePicker
+                                error={!!errors?.endDate}
+                                selected={field.value}
+                                errorMessage={errors?.endDate?.message}
+                                onChange={field.onChange}
+                                className='w-full'
+                                placeholderText='Data Final'
+
+                            />
+                        )}
+                />
+
+
             </div>
 
-            <Input className='mt-4' placeholder={`Número de hóspedes (max: ${trip.maxGuests})`} />
+            <Input {...register('guests', {
+                required: {
+                    value: true,
+                    message: "Número de hóspedes é obrigatório."
+                },
+            })}
+                error={!!errors?.guests}
+                errorMessage={errors?.guests?.message}
+                className='mt-4'
+                placeholder={`Número de hóspedes (max: ${trip.maxGuests})`}
+            />
 
             <div className="flex justify-between mt-3">
                 <p className='font-medium text-sm text-primaryDarker'>Total: </p>
@@ -28,7 +102,7 @@ const TripReservation = ({ trip }: TripReservationProps) => {
 
 
             <div className='p-10 border-b border-grayLighter w-full'>
-                <Button className='mt-3 w-full'>
+                <Button onClick={() => handleSubmit(onSubmit)()} className='mt-3 w-full'>
                     Reservar agora
                 </Button>
             </div>
