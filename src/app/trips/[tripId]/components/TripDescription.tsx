fa9a6c75ad41803077c77d2107728dced3e12d05
Leonardo <leondani853@gmail.com>
2023-07-08 23:46:44 -0300

    style: adapt trip details do desktop

--- src/app/trips/[tripId]/components/TripDescription.tsx
+++ src/app/trips/[tripId]/components/TripDescription.tsx
@@ -1,18 +1,16 @@
-import { Trip } from '@prisma/client';
-import React from 'react';
+import React from "react";
 
 interface TripDescriptionProps {
-    description: string
+  description: string;
 }
 
 const TripDescription = ({ description }: TripDescriptionProps) => {
-
-    return (
-        <div className='flex flex-col p-5'>
-            <h2 className='font-semibold text-primaryDarker'>Sobre a viagem</h2>
-            <p className='text-xs leading-5 text-primaryDarker mt-1'>{description}</p>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col p-5 lg:p-0">
+      <h2 className="font-semibold text-primaryDarker lg:text-xl">Sobre a viagem</h2>
+      <p className="text-xs leading-5 text-primaryDarker mt-1 lg:mt-5 lg:text-base lg:leading-7">{description}</p>
+    </div>
+  );
+};
 
 export default TripDescription;
